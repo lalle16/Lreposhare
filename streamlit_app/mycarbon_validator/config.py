10fb6222ef3383be33d220c48dd391f1f2dae066
LK <lars@daybreak.dk>
2025-09-19 14:39:04 +0200

    1

--- streamlit_app/mycarbon_validator/config.py
+++ streamlit_app/mycarbon_validator/config.py
@@ -5,7 +5,7 @@ APP_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DEFAULT_SCOPE = "Scope 1"
 DEFAULT_TABLE = "Scope1Calcs"
-
+CLIENT_DATA_PATH = os.path.dirname(os.path.abspath(__file__))
 
 def get_validations_excel_path() -> str:
     """Return the absolute path to validations.xlsx in the same folder as this app."""
