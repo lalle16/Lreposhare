d3b46a6a3918f9b4037bf8b3d3e36da196b07181
LK <lars@daybreak.dk>
2025-09-25 17:21:11 +0200

    s

--- streamlit_app/mycarbon_validator/config.py
+++ streamlit_app/mycarbon_validator/config.py
@@ -3,6 +3,6 @@ import os
 # Always point to the directory where this script lives
 client_data_path = os.path.dirname(os.path.abspath(__file__))
 
-def get_validations_excel_path():
+def get_validations_excel_path(client_data_path):
     """Return the absolute path to validations.xlsx in the same folder as this app."""
     return os.path.join(client_data_path, "validations.xlsx")
